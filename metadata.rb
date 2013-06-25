93e04c605715d8c583b5ba48d71fd72bdebf82cb
Maks <github.maks3w@virtualplanets.net>
2013-06-25 15:27:28 +0300

    Level up

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Maks3w'
 license          'MIT'
 description      'Installs/Configures Magento'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "apt"
 depends "apache2"
